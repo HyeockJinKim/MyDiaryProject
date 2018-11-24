084032ef609047631481e46318efc759826b1915
HyeockJinKim <kherootz@gmail.com>
2018-11-24 14:16:46 +0900

    blog의 모델 설정

--- Diary/settings.py
+++ Diary/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class WithMe(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.TextField()
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+
+
+class Diary(models.Model):
+    title = models.CharField(max_length=50)
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL)  # Null: 특별한 주제 없음
+    post = models.TextField()
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)  # Null: 알 수 없는 장소
+    with_me = models.ManyToManyField(WithMe)
+    created = models.DateTimeField(auto_now_add=True)
